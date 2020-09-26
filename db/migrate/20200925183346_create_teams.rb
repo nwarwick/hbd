61b0e29ebc005b15e483c148f5265923cb4975cd
nwarwick <nicholaswarwick@icloud.com>
2020-09-26 11:03:01 -0400

    feat(schema): update table columns

--- db/migrate/20200925183346_create_teams.rb
+++ db/migrate/20200925183346_create_teams.rb
@@ -1,8 +1,9 @@
 class CreateTeams < ActiveRecord::Migration[5.2]
   def change
     create_table :teams do |t|
-      t.string :name, null: false, limit: 320
-      t.string :token, null: false, unique: true, limit: 320
+      t.string :domain, null: false, unique: true, limit: 320
+      t.string :slack_id, null: false, unique: true, limit: 320
+
 
       t.timestamps
     end
