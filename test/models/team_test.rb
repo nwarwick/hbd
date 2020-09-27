8863811492a83cb450590f832c038caf25a81f7a
nwarwick <nicholaswarwick@icloud.com>
2020-09-27 11:40:52 -0400

    test(team): add model tests

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @team = Team.new(domain: 'fwelmfsdflknwe', slack_id: 'sdfsfds')
+  end
+
+  test 'valid team' do
+    assert @team.valid?
+  end
+
+  test 'invalid without slack_id' do
+    @team.slack_id = nil
+    assert_not @team.valid?
+  end
+
+  test 'invalid without domain' do
+    @team.domain = nil
+    assert_not @team.valid?
+  end
 end
