7b9c9fa4b8789d02837bf6942e6afeedcd51eac9
nwarwick <nicholaswarwick@icloud.com>
2020-09-27 22:23:19 -0400

    feat(user): add ability to filter users having birthday today

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
   belongs_to :team, optional: false
+
+  scope :born_today,
+        lambda {
+          where(
+            'EXTRACT(day FROM birthday) = ? AND EXTRACT(month FROM birthday) = ?',
+            Time.now.day,
+            Time.now.month
+          )
+        }
 end
