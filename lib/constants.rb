793c863c303c8bde303dd93be0533d13ab1fc14d
nwarwick <nicholaswarwick@icloud.com>
2020-10-10 13:36:02 -0400

    feat: allow birthday submission

--- lib/constants.rb
+++ lib/constants.rb
@@ -1,32 +1,16 @@
 # frozen_string_literal: true
 
 module Constants
-  # TODO: Update the response to display a modal along with a datepicker https://github.com/nwarwick/hbd/issues/8
   # TODO: Update the response to allow viewing of birthdays https://github.com/nwarwick/hbd/issues/9 (lower priority)
-  SLACK_COMMAND_RESPONSE = {
-    "text": 'How can I help you?',
-    "attachments": [
-      {
-        "fallback": 'You are unable to choose a game',
-        "callback_id": 'birthday_handler',
-        "color": '#3AA3E3',
-        "attachment_type": 'default',
-        "actions": [
-          {
-            "name": 'view_birthdays',
-            "text": 'View birthdays',
-            "type": 'button',
-            "value": 'view_birthdays'
-          },
-          {
-            "name": 'add_birthday',
-            "text": 'Add your birthday',
-            "style": 'primary',
-            "type": 'button',
-            "value": 'add_birthday'
-          }
-        ]
-      }
+  # TODO: use datepicker as input to prevent invalid birthday submissions
+  SLACK_DATEPICKER = {
+    "type": 'modal',
+    "callback_id": 'datepicker-form',
+    "title": { "type": 'HBD', "text": 'HBD', "emoji": true },
+    "submit": { "type": 'plain_text', "text": 'Submit', "emoji": true },
+    "close": { "type": 'plain_text', "text": 'Cancel', "emoji": true },
+    "elements": [
+      { "type": 'text', "label": 'Enter your birthday!', "name": 'birthday' }
     ]
   }.freeze
 end
