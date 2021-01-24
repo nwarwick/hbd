b7e01a099f0e124c5816142a8778fefd6fb18317
Nick Warwick <nicholaswarwick@icloud.com>
2021-01-24 12:55:08 -0500

    chore: formatting

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_925_185_145) do
+ActiveRecord::Schema.define(version: 2020_09_25_185145) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
@@ -30,7 +28,7 @@ ActiveRecord::Schema.define(version: 20_200_925_185_145) do
     t.datetime 'birthday', null: false
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
-    t.index ['team_id'], name: 'index_users_on_team_id'
+    t.index %w[team_id], name: 'index_users_on_team_id'
   end
 
   add_foreign_key 'users', 'teams'
