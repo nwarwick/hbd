d85d62909208dfcb6c280f1aabbf2809896ea6f0
Nick Warwick <nicholaswarwick@icloud.com>
2021-02-19 21:21:58 -0500

    chore: disable forgery protection

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Hbd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = false
+
+    config.action_controller.default_protect_from_forgery = false
   end
 end
